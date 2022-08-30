c456a349d5eb55e8f4643aae5df655909db00db5
杭 <1942499981@qq.com>
2022-08-30 22:29:43 +0800

    首页增加图片

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,22 +1,119 @@
 <template>
   <div class="dashboard-container">
+    <div class="container">
+      <div><img :src="a" alt="" /></div>
+      <div><img :src="b" alt="" /></div>
+      <div><img :src="c" alt="" /></div>
+      <div><img :src="d" alt="" /></div>
+      <div><img :src="e" alt="" /></div>
+      <div><img :src="f" alt="" /></div>
+    </div>
     <div class="dashboard-text">name: {{ name }}</div>
+    <!-- <div><img :src="require('@/assets/pic/1.png')" alt="" /></div> -->
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
+import { mapGetters } from "vuex";
+import a from "@/assets/pic/1.png";
+import b from "@/assets/pic/2.png";
+import c from "@/assets/pic/3.png";
+import d from "@/assets/pic/4.png";
+import e from "@/assets/pic/5.png";
+import f from "@/assets/pic/6.png";
 export default {
-  name: 'Dashboard',
+  name: "Dashboard",
+  data() {
+    return {
+      a: a,
+      b: b,
+      c: c,
+      d: d,
+      e: e,
+      f: f,
+    };
+  },
   computed: {
-    ...mapGetters([
-      'name'
-    ])
-  }
-}
-</script>
+    ...mapGetters(["name"]),
+  },
+  mounted() {
+    // 要操作的元素
+    const container = document.querySelector(".container");
+    const imgs = document.querySelectorAll(".container img");
 
+    // 为容器绑定鼠标移动事件
+    container.addEventListener("mousemove", function (e) {
+      // e.clientX 鼠标指针相对于窗口的水平坐标
+      // window.outerWidth 窗口的外部宽度
+      let percent = e.clientX / window.outerWidth;
+      // 计算水平偏移量
+      let offset = 10 * percent;
+      let blur = 20;
+
+      // 遍历图片
+      imgs.forEach((img, index) => {
+        offset *= 1.3;
+        // 计算模糊度
+        let blurValue = Math.pow(index / imgs.length - percent, 2) * blur;
+        // 将计算好的值赋给CSS的自定义属性
+        img.style.setProperty("--offset", offset + "px");
+        img.style.setProperty("--blur", blurValue + "px");
+      });
+    });
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  /* 100%窗口宽高 */
+  height: 100vh;
+  /* 弹性布局 水平+垂直居中 垂直排列 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.container {
+  width: 100%;
+  height: 160px;
+  /* 相对定位 */
+  position: relative;
+  /* 溢出隐藏 */
+  overflow: hidden;
+}
+.container div {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* 绝对定位 */
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+.container div img {
+  width: 110%;
+  height: 100%;
+  /* 对图片进行剪切，保留原始比例 */
+  object-fit: cover;
+  /* --offset（水平偏移量）、--blur（模糊度 数值越大越模糊）是CSS的自定义属性，可通过var函数进行调用 */
+  --offset: 0px;
+  --blur: 2px;
+  /* 设置水平偏移量 */
+  transform: translateX(var(--offset));
+  /* 设置模糊度 */
+  filter: blur(var(--blur));
+}
+h3 {
+  margin-top: 25px;
+}
+</style>
 <style lang="scss" scoped>
 .dashboard {
   &-container {
