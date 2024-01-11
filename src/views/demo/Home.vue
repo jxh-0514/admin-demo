af97ea30d7378c21a95aae2146041b2f7f832eda
杭 <1942499981@qq.com>
2024-01-12 00:48:06 +0800

    关闭flv-extend重连选项，解决视频地址错误时无法销毁播放器；更新西瓜flv视频地址

--- src/views/demo/Home.vue
+++ src/views/demo/Home.vue
@@ -4,27 +4,27 @@
  * @Author: 杭
  * @Date: 2022-08-23 20:06:43
  * @LastEditors: 杭
- * @LastEditTime: 2023-09-02 15:25:40
+ * @LastEditTime: 2024-01-11 23:38:37
 -->
 <template>
-  <div class="home"></div>
+	<div class="home">home</div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-  data() {
-    return {};
-  },
-  components: {},
-  methods: {},
+	name: "Home",
+	data() {
+		return {};
+	},
+	components: {},
+	methods: {},
 };
 </script>
 
 <style lang="less">
 .home {
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
 }
 </style>
