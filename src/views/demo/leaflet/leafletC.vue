1e8a4953a68ddff89db01a9143ef10ff924c72f9
杭 <1942499981@qq.com>
2024-03-04 22:11:40 +0800

    增加leaflet铺满视口页面

--- src/views/demo/leaflet/leafletC.vue
+++ src/views/demo/leaflet/leafletC.vue
@@ -1,32 +1,18 @@
 <template>
-  <div class="vue-leaflet-container">
-    <l-map
-      id="lmap"
-      ref="map"
-      style="width: 100%; height: 94vh"
-      :zoom="zoom"
-      :center="center"
-    >
-      <l-tile-layer :url="url" :attribution="attribution" />
-      <!-- <v-marker-cluster> -->
-      <template v-for="(marker, index) in markers">
-        <l-marker
-          :key="index"
-          :icon="defaultIcon"
-          :lat-lng="[marker.lat, marker.lng]"
-          @click="handleMarkerClick(marker)"
-        >
-          <!-- <l-popup :content="cardTemplate" :options="popupOptions" /> -->
-          <!-- 优化 -->
-          <l-popup
-            :content="cardTemplate[0] + marker.id + cardTemplate[1]"
-            :options="popupOptions"
-          />
-        </l-marker>
-      </template>
-      <!-- </v-marker-cluster>  -->
-    </l-map>
-  </div>
+	<div class="vue-leaflet-container">
+		<l-map id="lmap" ref="map" style="width: 100%; height: 94vh" :zoom="zoom" :center="center">
+			<l-tile-layer :url="url" :attribution="attribution" />
+			<!-- <v-marker-cluster> -->
+			<template v-for="(marker, index) in markers">
+				<l-marker :key="index" :icon="defaultIcon" :lat-lng="[marker.lat, marker.lng]" @click="handleMarkerClick(marker)">
+					<!-- <l-popup :content="cardTemplate" :options="popupOptions" /> -->
+					<!-- 优化 -->
+					<l-popup :content="cardTemplate[0] + marker.id + cardTemplate[1]" :options="popupOptions" />
+				</l-marker>
+			</template>
+			<!-- </v-marker-cluster>  -->
+		</l-map>
+	</div>
 </template>
 <script>
 import L from "leaflet";
@@ -37,119 +23,116 @@ import icon from "leaflet/dist/images/marker-icon-2x.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
 import Pane from "../leafletComponents/PopupContent";
 export default {
-  name: "LeafletC",
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-    LPopup,
-    "v-marker-cluster": Vue2LeafletMarkerCluster,
-    Pane,
-  },
-  data() {
-    return {
-      zoom: 5,
-      center: [39.90553, 116.391305],
-      url: "https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png",
-      attribution:
-        "&copy; <a href='http://osm.org/copyrighte'>OpenStreetMap</a> contributors",
-      markers: [],
-      defaultIcon: L.icon({
-        iconUrl: icon,
-        iconSize: [26, 42],
-        iconAnchor: [13, 42],
-        shadowUrl: iconShadow,
-        shadowSize: [41, 41],
-        shadowAnchor: [13, 41],
-      }),
-      // cardTemplate:
-      //   "<div id='pane' style='width: 300px; height: 240px;'></div>",
-      // 优化
-      cardTemplate: [
-        '<div style="width: 300px; height: 240px;" id="pane_',
-        '" />',
-      ],
-      popupOptions: {
-        className: "mypopup",
-      },
-      pane: null,
-      stationAlarmNum: { 1: 10, 2: 8, 3: 12 },
-    };
-  },
-  created() {
-    // this.setMarkers();
-  },
-  mounted() {
-    this.setMarkers();
-    console.log("====================================");
-    console.log(123, this.$refs.map);
-    console.log("====================================");
-    var that = this;
-    this.$refs.map.mapObject.on("popupopen", (e) => {
-      // this.pane.$mount("#pane");
-      // 优化
-      console.log("监听弹窗");
-      // 加定时器解决多次点击播放器报错问题
-      setTimeout(() => {
-        that.pane.$mount("#pane_" + that.pane.id);
-      }, 200);
-    });
-  },
-  methods: {
-    setMarkers() {
-      this.$nextTick(() => {
-        let arr = [
-          {
-            id: 1,
-            name: "测试站点1",
-            lat: 39.90553,
-            lng: 116.391305,
-            data: "http://192.168.1.71:8080/hdl/34020000001110000002/34020000001310000003.flv",
-          },
-          {
-            id: 2,
-            name: "测试站点2",
-            lat: 40.90553,
-            lng: 116.393305,
-            data: "http://192.168.1.71:8080/hls/34020000001110000002/34020000001310000003.m3u8",
-          },
-          { id: 3, name: "测试站点3", lat: 41.90553, lng: 116.395305 },
-        ];
-        this.markers = arr;
-      });
-    },
-    handleMarkerClick(station) {
-      console.log("点击", station);
-      if (this.pane != null) {
-        console.log("销毁弹窗");
-        // this.$refs.map.mapObject.closePopup();
-        this.pane.$destroy();
-        this.pane = null;
-      }
-      var Component = Vue.extend(Pane);
-      this.pane = new Component({ propsData: { popupData: station.data } });
-      this.pane.alarmNum = this.stationAlarmNum[station.id];
-      this.pane.name = station.name;
-      this.pane.id = station.id;
-      // setTimeout(() => {
-      //   this.pane.$mount("#pane_" + this.pane.id);
-      // }, 200);
-      // console.log("构造完成");
-    },
-    // setMarkers() {
-    //   fetch(
-    //     "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_populated_places.geojson"
-    //   )
-    //     .then((response) => {
-    //       return response.json();
-    //     })
-    //     .then((json) => {
-    //       this.markers = json.features.slice(0, 10).map((x) => {
-    //         return x.properties;
-    //       });
-    //     });
-    // },
-  },
+	name: "LeafletC",
+	components: {
+		LMap,
+		LTileLayer,
+		LMarker,
+		LPopup,
+		"v-marker-cluster": Vue2LeafletMarkerCluster,
+		Pane,
+	},
+	data() {
+		return {
+			zoom: 5,
+			center: [39.90553, 116.391305],
+			url: "https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png",
+			attribution: "&copy; <a href='http://osm.org/copyrighte'>OpenStreetMap</a> contributors",
+			markers: [],
+			defaultIcon: L.icon({
+				iconUrl: icon,
+				iconSize: [26, 42],
+				iconAnchor: [13, 42],
+				shadowUrl: iconShadow,
+				shadowSize: [41, 41],
+				shadowAnchor: [13, 41],
+			}),
+			// cardTemplate:
+			//   "<div id='pane' style='width: 300px; height: 240px;'></div>",
+			// 优化
+			cardTemplate: ['<div style="width: 300px; height: 240px;" id="pane_', '" />'],
+			popupOptions: {
+				className: "mypopup",
+			},
+			pane: null,
+			stationAlarmNum: { 1: 10, 2: 8, 3: 12 },
+		};
+	},
+	created() {
+		// this.setMarkers();
+	},
+	mounted() {
+		this.setMarkers();
+		console.log("====================================");
+		console.log(123, this.$refs.map);
+		console.log("====================================");
+		var that = this;
+		this.$refs.map.mapObject.on("popupopen", (e) => {
+			// this.pane.$mount("#pane");
+			// 优化
+			console.log("监听弹窗");
+			// 加定时器解决多次点击播放器报错问题
+			setTimeout(() => {
+				that.pane.$mount("#pane_" + that.pane.id);
+			}, 200);
+		});
+	},
+	methods: {
+		setMarkers() {
+			this.$nextTick(() => {
+				let arr = [
+					{
+						id: 1,
+						name: "测试站点1",
+						lat: 39.90553,
+						lng: 116.391305,
+						data: "https://sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/flv/xgplayer-demo-360p.flv",
+					},
+					{
+						id: 2,
+						name: "测试站点2",
+						lat: 40.90553,
+						lng: 116.393305,
+						data: "https://tutorialehtml.com/assets_tutorials/media/Shaun-the-Sheep-The-Movie-Official-Trailer.mp4",
+					},
+					{ id: 3, name: "测试站点3", lat: 41.90553, lng: 116.395305 },
+				];
+				this.markers = arr;
+			});
+		},
+		handleMarkerClick(station) {
+			console.log("点击", station);
+			if (!station.data) return this.$message.warning("暂无视频地址");
+			if (this.pane != null) {
+				console.log("销毁弹窗");
+				// this.$refs.map.mapObject.closePopup();
+				this.pane.$destroy();
+				this.pane = null;
+			}
+			var Component = Vue.extend(Pane);
+			this.pane = new Component({ propsData: { popupData: station.data } });
+			this.pane.alarmNum = this.stationAlarmNum[station.id];
+			this.pane.name = station.name;
+			this.pane.id = station.id;
+			// setTimeout(() => {
+			//   this.pane.$mount("#pane_" + this.pane.id);
+			// }, 200);
+			// console.log("构造完成");
+		},
+		// setMarkers() {
+		//   fetch(
+		//     "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_populated_places.geojson"
+		//   )
+		//     .then((response) => {
+		//       return response.json();
+		//     })
+		//     .then((json) => {
+		//       this.markers = json.features.slice(0, 10).map((x) => {
+		//         return x.properties;
+		//       });
+		//     });
+		// },
+	},
 };
 </script>
 <style>
--- src/views/demo/leafletComponents/PopupContent.vue
+++ src/views/demo/leafletComponents/PopupContent.vue
@@ -1,45 +1,38 @@
 <!-- leaflet  使用Vue,extend构造组件 -->
 <template>
-  <div>
-    <!--这里写需要的弹窗内容 -->
-    <!-- <el-carousel width="200px" height="150px">
+	<div>
+		<!--这里写需要的弹窗内容 -->
+		<!-- <el-carousel width="200px" height="150px">
       <el-carousel-item v-for="item in 3" :key="item">
         <h3 class="small">{{ item }}</h3>
       </el-carousel-item>
     </el-carousel> -->
-    <!-- <div id="staionPane">
+		<!-- <div id="staionPane">
       <div id="stationName">{{ name }}</div>
       <div>
         <span>告警数量：</span>
         <a id="alarmNum" @click="handleClick">{{ alarmNum }}</a>
       </div>
     </div> -->
-    <!-- echarts实例 -->
-    <!-- <div id="echarts" style="width: 300px; height: 200px"></div> -->
-    <!-- 视频测试 -->
-    <!-- <video
+		<!-- echarts实例 -->
+		<!-- <div id="echarts" style="width: 300px; height: 200px"></div> -->
+		<!-- 视频测试 -->
+		<!-- <video
       style="width: 300px; height: 200px"
       src="https://tutorialehtml.com/assets_tutorials/media/Shaun-the-Sheep-The-Movie-Official-Trailer.mp4"
     ></video> -->
-    <!-- <Video
+		<!-- <Video
       src="https://tutorialehtml.com/assets_tutorials/media/Shaun-the-Sheep-The-Movie-Official-Trailer.mp4"
       :videoData="true"
     ></Video> -->
-    <video
-      id="videoElement"
-      controls
-      autoplay
-      muted
-      width="300px"
-      height="200px"
-    ></video>
-    <button @click="play">加速</button>
-    <!--SkeyeWebPlayer播放器容器-->
-    <!-- <div id="WebMediaPlayer" style="width: 300px; height: 240px"></div>
+		<video id="videoElement" controls autoplay muted width="300px" height="200px"></video>
+		<button @click="play">加速</button>
+		<!--SkeyeWebPlayer播放器容器-->
+		<!-- <div id="WebMediaPlayer" style="width: 300px; height: 240px"></div>
     <button @click="handlePlay">播放</button> -->
-    <!-- 腾讯web播放器 -->
-    <!-- <div id="tcplayer"></div> -->
-  </div>
+		<!-- 腾讯web播放器 -->
+		<!-- <div id="tcplayer"></div> -->
+	</div>
 </template>
 
 <script>
@@ -48,245 +41,239 @@ import Video from "@/components/Video";
 import flvjs from "flv.js";
 import mpegts from "mpegts.js";
 export default {
-  name: "PopupContent",
-  props: {
-    // 接收传来的数据
-    popupData: {
-      // type: Object,
-      // default: () => {},
-      type: String,
-      default: "",
-    },
-  },
-  components: {
-    Video,
-  },
+	name: "PopupContent",
+	props: {
+		// 接收传来的数据
+		popupData: {
+			// type: Object,
+			// default: () => {},
+			type: String,
+			default: "",
+		},
+	},
+	components: {
+		Video,
+	},
 
-  data() {
-    return {
-      name: "",
-      id: null,
-      alarmNum: 0,
-      options: {
-        xAxis: {
-          type: "category",
-          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-        },
-        yAxis: {
-          type: "value",
-        },
-        series: [
-          {
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
-            type: "line",
-            smooth: true,
-          },
-        ],
-      },
-      flvPlayer: null,
-      palyer: null, //SkeyeWebPlayer播放器 / 腾讯web播放器
-    };
-  },
+	data() {
+		return {
+			name: "",
+			id: null,
+			alarmNum: 0,
+			options: {
+				xAxis: {
+					type: "category",
+					data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+				},
+				yAxis: {
+					type: "value",
+				},
+				series: [
+					{
+						data: [820, 932, 901, 934, 1290, 1330, 1320],
+						type: "line",
+						smooth: true,
+					},
+				],
+			},
+			flvPlayer: null,
+			palyer: null, //SkeyeWebPlayer播放器 / 腾讯web播放器
+		};
+	},
 
-  computed: {},
+	computed: {},
 
-  watch: {},
+	watch: {},
 
-  //生命周期 - 创建完成（可以访问当前this实例）
-  created() {},
-  //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {
-    // this.initEchats();
-    // flv播放器
-    // this.initFlv();
-    // mpegts.js播放器
-    this.initMpegts();
-    // SkeyeWebPlayer 播放器
-    // this.$nextTick(() => {
-    //   this.initPlayer();
-    //   this.handlePlay();
-    // });
-    // 腾讯web播放器
-    // this.initplayer();
-    console.log("接收", this.popupData);
-  },
+	//生命周期 - 创建完成（可以访问当前this实例）
+	created() {},
+	//生命周期 - 挂载完成（可以访问DOM元素）
+	mounted() {
+		// this.initEchats();
+		// flv播放器
+		// this.initFlv();
+		// mpegts.js播放器
+		this.initMpegts();
+		// SkeyeWebPlayer 播放器
+		// this.$nextTick(() => {
+		//   this.initPlayer();
+		//   this.handlePlay();
+		// });
+		// 腾讯web播放器
+		// this.initplayer();
+		console.log("接收", this.popupData);
+	},
 
-  methods: {
-    handleClick() {
-      alert(this.alarmNum);
-    },
-    initEchats() {
-      var chartDom = document.getElementById("echarts");
-      var myChart = echarts.init(chartDom);
-      this.options && myChart.setOption(this.options);
-    },
-    //flv播放器
-    initFlv() {
-      if (flvjs.isSupported()) {
-        var videoElement = document.getElementById("videoElement");
-        this.flvPlayer = flvjs.createPlayer({
-          type: "flv",
-          isLive: true,
-          hasAudio: false,
-          url: "http://192.168.1.222:8080/hdl/34020000001110000002/34020000001310000003.flv", // 自己的flv视频流
-        });
-        this.flvPlayer.attachMediaElement(videoElement);
-        this.flvPlayer.load();
-        // this.flvPlayer.play();
-        var playPromise = this.flvPlayer.play();
-        if (playPromise !== undefined) {
-          playPromise
-            .then((_) => {
-              console.log("播放", _);
-              // Automatic playback started!
-              // Show playing UI.
-              this.flvPlayer.play();
-            })
-            .catch((error) => {
-              console.log("暂停", error);
-              // Auto-play was prevented
-              // Show paused UI.
-              // this.muted = false;
-              // 销毁flv
-              this.flvPlayer.pause();
-              this.flvPlayer.unload();
-              this.flvPlayer.detachMediaElement();
-              this.flvPlayer.destroy();
-              this.flvPlayer = null;
-              // 重新加载
-              this.initFlv();
-            });
-        }
-      }
-    },
-    play() {
-      // this.flvPlayer.play();
-    },
-    // mpegts播放器
-    initMpegts() {
-      if (mpegts.getFeatureList().mseLivePlayback) {
-        var videoElement = document.getElementById("videoElement");
-        this.flvPlayer = mpegts.createPlayer({
-          type: "flv", // could also be mpegts, m2ts, flv
-          isLive: true,
-          url: this.popupData,
-        });
-        this.flvPlayer.attachMediaElement(videoElement);
-        this.flvPlayer.load();
-        this.flvPlayer.play();
-        var playPromise = this.flvPlayer.play();
-        if (playPromise !== undefined) {
-          playPromise
-            .then((_) => {
-              console.log("播放", _);
-              // Automatic playback started!
-              // Show playing UI.
-              // this.flvPlayer.play();
-            })
-            .catch((error) => {
-              console.log("暂停", error);
-              // Auto-play was prevented
-              // Show paused UI.
-              // this.muted = false;
-              // 销毁flv
-              this.flvPlayer.pause();
-              this.flvPlayer.unload();
-              this.flvPlayer.detachMediaElement();
-              this.flvPlayer.destroy();
-              this.flvPlayer = null;
-              // 重新加载
-              this.initFlv();
-            });
-        }
-      }
-    },
-    //===============SkeyeWebPlayer播放器===============
-    initPlayer() {
-      this.player = new WebMediaPlayer(
-        "",
-        `WebMediaPlayer`,
-        this.callbackFunc,
-        {
-          cbUserPtr: this,
-          decodeType: "auto",
-          height: true,
-        }
-      );
-    },
-    // 播放器回调方法
-    callbackFunc(cbType, cbParams) {
-      if (cbType === "playbackTime") {
-        //console.log("当前回放时间: " + cbParams);
-      } else if (cbType === "ended") {
-        console.log("播放结束");
-      }
-      console.log("Callback " + cbType + ":  " + cbParams);
-    },
-    // 播放
-    handlePlay() {
-      let url =
-        "http://192.168.1.222:8080/hdl/34020000001110000002/34020000001310000003.flv";
-      if (this.player) {
-        this.player.destroy();
-        this.initPlayer();
-        this.player.play(url, 1, 0);
-      }
-    },
-    handleClick(type) {
-      if (type === "stop") {
-        this.player.stop();
-      }
-    },
-    // 腾讯web播放器
-    initplayer() {
-      if (this.player) {
-        this.player.destroy();
-        this.player = null;
-      }
-      var breakTime = 60 * 10;
-      this.player = new TcPlayer("tcplayer", {
-        m3u8: this.popupData,
-        flv: this.popupData,
-        autoplay: true, //iOS 下 safari 浏览器，以及大部分移动端浏览器是不开放视频自动播放这个能力的
-        //"poster": this.anchor.cover,
-        width: "100%", //480视频的显示宽度，请尽量使用视频分辨率宽度
-        height: "180", //320视频的显示高度，请尽量使用视频分辨率高度
-        wording: {
-          2: "未直播",
-          2032: "未直播", //"请求视频失败，请检查网络",
-          2048: "未直播", // "请求m3u8文件失败，可能是网络错误或者跨域问题"
-        },
-        listener(msg) {
-          // if (this.player.currentTime() > breakTime) {
-          //   //播放，且超过10分钟，暂停且提示！
-          //   player.pause();
-          //   // that.tipVisible = true;
-          //   console.log("播放超过10分钟");
-          // }
-        },
-      });
-    },
-  },
+	methods: {
+		handleClick() {
+			alert(this.alarmNum);
+		},
+		initEchats() {
+			var chartDom = document.getElementById("echarts");
+			var myChart = echarts.init(chartDom);
+			this.options && myChart.setOption(this.options);
+		},
+		//flv播放器
+		initFlv() {
+			if (flvjs.isSupported()) {
+				var videoElement = document.getElementById("videoElement");
+				this.flvPlayer = flvjs.createPlayer({
+					type: "flv",
+					isLive: true,
+					hasAudio: false,
+					url: "http://192.168.1.222:8080/hdl/34020000001110000002/34020000001310000003.flv", // 自己的flv视频流
+				});
+				this.flvPlayer.attachMediaElement(videoElement);
+				this.flvPlayer.load();
+				// this.flvPlayer.play();
+				var playPromise = this.flvPlayer.play();
+				if (playPromise !== undefined) {
+					playPromise
+						.then((_) => {
+							console.log("播放", _);
+							// Automatic playback started!
+							// Show playing UI.
+							this.flvPlayer.play();
+						})
+						.catch((error) => {
+							console.log("暂停", error);
+							// Auto-play was prevented
+							// Show paused UI.
+							// this.muted = false;
+							// 销毁flv
+							this.flvPlayer.pause();
+							this.flvPlayer.unload();
+							this.flvPlayer.detachMediaElement();
+							this.flvPlayer.destroy();
+							this.flvPlayer = null;
+							// 重新加载
+							this.initFlv();
+						});
+				}
+			}
+		},
+		play() {
+			// this.flvPlayer.play();
+		},
+		// mpegts播放器
+		initMpegts() {
+			if (mpegts.getFeatureList().mseLivePlayback) {
+				var videoElement = document.getElementById("videoElement");
+				this.flvPlayer = mpegts.createPlayer({
+					type: "flv", // could also be mpegts, m2ts, flv
+					isLive: true,
+					url: this.popupData,
+				});
+				this.flvPlayer.attachMediaElement(videoElement);
+				this.flvPlayer.load();
+				this.flvPlayer.play();
+				var playPromise = this.flvPlayer.play();
+				if (playPromise !== undefined) {
+					playPromise
+						.then((_) => {
+							console.log("播放", _);
+							// Automatic playback started!
+							// Show playing UI.
+							// this.flvPlayer.play();
+						})
+						.catch((error) => {
+							console.log("暂停", error);
+							// Auto-play was prevented
+							// Show paused UI.
+							// this.muted = false;
+							// 销毁flv
+							this.flvPlayer.pause();
+							this.flvPlayer.unload();
+							this.flvPlayer.detachMediaElement();
+							this.flvPlayer.destroy();
+							this.flvPlayer = null;
+							// 重新加载
+							this.initFlv();
+						});
+				}
+			}
+		},
+		//===============SkeyeWebPlayer播放器===============
+		initPlayer() {
+			this.player = new WebMediaPlayer("", `WebMediaPlayer`, this.callbackFunc, {
+				cbUserPtr: this,
+				decodeType: "auto",
+				height: true,
+			});
+		},
+		// 播放器回调方法
+		callbackFunc(cbType, cbParams) {
+			if (cbType === "playbackTime") {
+				//console.log("当前回放时间: " + cbParams);
+			} else if (cbType === "ended") {
+				console.log("播放结束");
+			}
+			console.log("Callback " + cbType + ":  " + cbParams);
+		},
+		// 播放
+		handlePlay() {
+			let url = "http://192.168.1.222:8080/hdl/34020000001110000002/34020000001310000003.flv";
+			if (this.player) {
+				this.player.destroy();
+				this.initPlayer();
+				this.player.play(url, 1, 0);
+			}
+		},
+		handleClick(type) {
+			if (type === "stop") {
+				this.player.stop();
+			}
+		},
+		// 腾讯web播放器
+		initplayer() {
+			if (this.player) {
+				this.player.destroy();
+				this.player = null;
+			}
+			var breakTime = 60 * 10;
+			this.player = new TcPlayer("tcplayer", {
+				m3u8: this.popupData,
+				flv: this.popupData,
+				autoplay: true, //iOS 下 safari 浏览器，以及大部分移动端浏览器是不开放视频自动播放这个能力的
+				//"poster": this.anchor.cover,
+				width: "100%", //480视频的显示宽度，请尽量使用视频分辨率宽度
+				height: "180", //320视频的显示高度，请尽量使用视频分辨率高度
+				wording: {
+					2: "未直播",
+					2032: "未直播", //"请求视频失败，请检查网络",
+					2048: "未直播", // "请求m3u8文件失败，可能是网络错误或者跨域问题"
+				},
+				listener(msg) {
+					// if (this.player.currentTime() > breakTime) {
+					//   //播放，且超过10分钟，暂停且提示！
+					//   player.pause();
+					//   // that.tipVisible = true;
+					//   console.log("播放超过10分钟");
+					// }
+				},
+			});
+		},
+	},
 };
 </script>
 <style lang="scss" scoped>
 .el-carousel__item h3 {
-  color: #475669;
-  font-size: 14px;
-  opacity: 0.75;
-  line-height: 150px;
-  margin: 0;
+	color: #475669;
+	font-size: 14px;
+	opacity: 0.75;
+	line-height: 150px;
+	margin: 0;
 }
 
 .el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+	background-color: #99a9bf;
 }
 
 .el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+	background-color: #d3dce6;
 }
 
 #stationName {
-  margin-top: 20px;
+	margin-top: 20px;
 }
 </style>
