f0cc78cb847569995fd080c2f5f1a059c10f07ad
jxh <1942499981@qq.com>
2024-06-14 16:04:42 +0800

    修改webpack配置首行缩进路径

--- vue.config.js
+++ vue.config.js
@@ -82,7 +82,7 @@ module.exports = {
 		resolve: {
 			alias: {
 				"@": resolve("src"),
-				"tinymce-plugins-indent2em": path.resolve(__dirname, "node_modules/tinymce/plugins/indent2em"),
+				"tinymce-plugins-indent2em": path.resolve(__dirname, "./node_modules/tinymce/plugins/indent2em"),
 				// 'cesium': path.resolve(__dirname, cesiumSource)
 			},
 		},
